6c2db4fccc77c4a59b111a32d598bf7c4ffdb9c2
PusiaPaker <antek.chelstowski@gmail.com>
2026-02-05 16:37:38 -0500

    not working auth yet, need to read more on it

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from app.routes.dashboard import DashBP
 from app.routes.sql_testing import TestBP
+from app.routes.auth import AuthBP
 from app.config import Config
 from app.src.database import db
 
@@ -10,12 +12,14 @@ from app.src.database import db
 def create_app():
     app = Flask(__name__, template_folder='./templates/')
     app.config.from_object(Config)
+    Session(app)
 
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
 
+    app.register_blueprint(AuthBP)
     app.register_blueprint(DashBP, url_prefix='/dashboard/')
     app.register_blueprint(TestBP, url_prefix='/sql/')
 
--- app/config.py
+++ app/config.py
@@ -1,2 +1,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = 'dev' # !!! DO NOT COMMIT WITH THIS ANYTHING ELSE BUT DEV
+    #   https://www.geeksforgeeks.org/python/how-to-use-flask-session-in-python-flask/
+    SESSION_PERMANENT = False     # Sessions expire when the browser is closed
+    SESSION_TYPE = 'filesystem'     # Store session data in files
