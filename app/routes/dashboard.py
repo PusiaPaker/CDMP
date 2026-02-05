9badea1580a907ac572133dd1564ba1ed755ea80
PusiaPaker <antek.chelstowski@gmail.com>
2026-02-05 15:55:42 -0500

    quick change to swith the routing to the one in config and not the files itself

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 DashBP= Blueprint('user', __name__)
 
-@DashBP.route('/dashboard')
+@DashBP.route('/')
 def get_dashboard():
     return render_template("dashboard/dashboard_index.html"), 200
 
