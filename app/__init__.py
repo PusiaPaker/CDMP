484fc0c8494492a4d45b343d23fda44a63c83c81
PusiaPaker <antek.achelstowski@gmail.com>
2026-02-18 18:51:41 -0500

    Addeed a 404 page to the project

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
@@ -10,7 +10,6 @@ from app.config import Config
 from app.src.database import db
 from app.src.commands import CommandsBP
 
-
 def create_app():
     app = Flask(__name__, template_folder='./templates/')
     app.config.from_object(Config)
@@ -20,6 +19,10 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template("error/404.html"), 404
 
     app.register_blueprint(AuthBP)
     app.register_blueprint(DashBP, url_prefix='/dashboard/')
