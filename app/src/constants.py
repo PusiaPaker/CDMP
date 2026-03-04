fcd73e03534fb4bf33c9d8bf8deb1a9e8e178983
Miles Keffer <mkeffer4@kent.edu>
2026-03-04 15:39:43 -0500

    functioning timeline
    lmk if you want any changes

--- app/src/constants.py
+++ app/src/constants.py
@@ -2,5 +2,5 @@
 # File for us to store some global constants
 #
 
-ALLOWED_FILE_EXTENSIONS = ['docx', 'pdf', 'docx', 'csv', 'xlsx']
+ALLOWED_FILE_EXTENSIONS = ['docx', 'pdf', 'csv', 'xlsx', 'xls']
 ALLOWED_FILE_CATEGORIES = ['unspecified', 'text', 'image', 'spreadsheet']
